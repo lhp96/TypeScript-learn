cbd921b46664284b154142b3561803f97bf7d2ff
luhuipeng <973880264@qq.com>
2022-12-03 15:41:25 +0800

    fix: readonly2

--- src/type-challenges/medium/Readonly2/template.ts
+++ src/type-challenges/medium/Readonly2/template.ts
@@ -4,5 +4,9 @@ type MyReadonly2<T, K extends keyof T = keyof T> = {
     [Q in keyof T as (Q extends K ? never : Q)]: T[Q]
 }
 
+// type MyReadonly2<T, K extends keyof T = keyof T> =
+//   Omit<T, K> & {
+//     +readonly [P in K]: T[P]
+//   }
 
-// 2个先后不能对调 一个加readonly，第二个其实是Omit
+// 2个先后 可以对调  一个加readonly，第二个其实是Omit
