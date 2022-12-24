6b3bd7ff14cf101349c0356b4f4916b7cb17328a
luhuipeng <973880264@qq.com>
2022-12-24 19:29:42 +0800

    fix: medium Append Argument

--- src/type-challenges/medium/Append Argument/template.ts
+++ src/type-challenges/medium/Append Argument/template.ts
@@ -1,5 +1,9 @@
 /* _____________ Your Code Here _____________ */
 
-type AppendArgument<Fn extends (...args: any[]) => any, A> = Fn extends (...args: infer P) => any
-  ? (...args: [...P, A]) => ReturnType<Fn>
-  : never
+// type AppendArgument<Fn extends (...args: any[]) => any, A> = Fn extends (...args: infer P) => any
+//   ? (...args: [...P, A]) => ReturnType<Fn>
+//   : never
+
+type AppendArgument<Fn extends Function, A> = Fn extends (...args: infer Args) => infer ReturnType
+  ? (...args: [...Args, A]) => ReturnType
+  : Fn
