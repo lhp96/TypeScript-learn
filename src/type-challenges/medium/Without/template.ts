caa5e2dc2c786eedd31f2822eb98e9a1862f38e7
luhuipeng <973880264@qq.com>
2022-12-29 22:35:33 +0800

    fix: Without  F2

--- src/type-challenges/medium/Without/template.ts
+++ src/type-challenges/medium/Without/template.ts
@@ -1,9 +1,17 @@
 /* _____________ Your Code Here _____________ */
-type WithoutOne<T, U> = T extends [infer P, ...infer Q]
-  ? [...(P extends U ? []: [P]), ...WithoutOne<Q, U>]  
-  : T
+// type WithoutOne<T, U> = T extends [infer P, ...infer Q]
+//   ? [...(P extends U ? []: [P]), ...WithoutOne<Q, U>]  
+//   : T
 
-type Without<T extends any[], U>
-  = U extends [infer P, ...infer Q]
-    ? Without<WithoutOne<T, P>, Q>
-    : WithoutOne<T, U>
+// type Without<T extends any[], U>
+//   = U extends [infer P, ...infer Q]
+//     ? Without<WithoutOne<T, P>, Q>
+//     : WithoutOne<T, U>
+
+// 新的解决方法
+type Without<T, U>
+  = T extends [infer P, ...infer Q]
+    ? P extends (U extends unknown[] ? U[number] : U)
+      ? Without<Q, U> 
+      : [P, ...Without<Q, U>]
+    : T
